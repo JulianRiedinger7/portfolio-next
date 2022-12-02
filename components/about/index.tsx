161f337e9a6100b18befa0f4262cfa04c9af8a55
Julian Riedinger <julianriedinger27@gmail.com>
2022-12-02 16:50:15 -0300

    [feature] - added about template & mobile nav menu

---
+++ components/about/index.tsx
@@ -0,0 +1,20 @@
+export default function About() {
+	return (
+		<section className="pb-20">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 200">
+				<path
+					fill="#0099ff"
+					fillOpacity="0.7"
+					d="M0,160L80,170.7C160,181,320,203,480,176C640,149,800,75,960,42.7C1120,11,1280,21,1360,26.7L1440,32L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+				></path>
+			</svg>
+			<h2 className="text-center">Sobre Mi</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum nostrum
+				ut ipsam, reprehenderit veniam commodi placeat, corporis itaque autem
+				aliquid dignissimos iusto dicta expedita assumenda odit recusandae
+				perspiciatis voluptatem? Laudantium?
+			</p>
+		</section>
+	);
+}
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image';
+import { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
 
 export default function Header() {
+	const [menuOpened, setMenuOpened] = useState<boolean>(false);
+
+	const onHandleMenu = () => setMenuOpened(!menuOpened);
+
 	return (
-		<header className="fixed top-0 flex items-center justify-between w-full h-20 shadow-xl lg:px-4">
-			<Image src="/logo.png" alt="vercel" width={120} height={120} />
+		<header className="fixed top-0 z-10 flex items-center justify-between w-full h-20 px-3 shadow-xl backdrop-blur-sm lg:px-5">
+			<Image src="/logo.png" alt="logo jr" width={120} height={120} />
 			<nav>
-				<ul className="flex space-x-4 text-xl">
+				<ul className="hidden space-x-4 text-xl md:flex">
 					<li>
 						<a href="#">Home</a>
 					</li>
@@ -16,6 +23,29 @@ export default function Header() {
 						<a href="#">Projects</a>
 					</li>
 				</ul>
+
+				<FaBars
+					size={30}
+					className="cursor-pointer md:hidden"
+					onClick={onHandleMenu}
+				/>
+				<div
+					className={
+						menuOpened
+							? 'absolute top-0 left-0 w-screen h-screen transition-all duration-300 ease-in-out bg-black/70 md:hidden'
+							: 'absolute left-[-100%]'
+					}
+				>
+					<div className="w-[70%] h-full bg-[#F1FAEE] p-5">
+						<div className="flex items-center justify-between">
+							<h3>JulianRiedinger</h3>
+							<AiOutlineClose size={30} onClick={onHandleMenu} />
+						</div>
+						<div className="border-b border-[#1D3557] my-2 pb-2">
+							<h4>Construyamos algo juntos!</h4>
+						</div>
+					</div>
+				</div>
 			</nav>
 		</header>
 	);
